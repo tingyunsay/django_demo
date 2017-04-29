9b1e939b4e3ba71de7140fdd80e9b6baf51f1145
tingyunsay <13284638588@163.com>
2017-04-29 15:49:06 +0800

    编写词库扩充工具.

--- tingyun/hong/record.py
+++ tingyun/hong/record.py
@@ -5,9 +5,9 @@ import time
 def record(name,request):
 	f = open('./hong/log.info','a+')
 	if request.META.has_key('HTTP_X_FORWARDED_FOR'):
-		print >> f,"at time %s , the ua is : %s , the access ip : %s , keyword is : %s".encode('utf-8')%(time.ctime(),request.META.get('HTTP_USER_AGENT',None),request.META['HTTP_X_FORWARDED_FOR'].encode('utf-8'),name)
+		print >> f,"at time %s , the ua is : %s , the access ip : %s:%s , keyword is : %s".encode('utf-8')%(time.ctime(),request.META.get('HTTP_USER_AGENT',None),request.META['HTTP_X_FORWARDED_FOR'],request.META.get('SERVER_PORT',None),name)
 	else:
-		print >> f,"at time %s , the ua is : %s , the access ip : %s , keyword is : %s".encode('utf-8')%(time.ctime(),request.META.get('HTTP_USER_AGENT',None),request.META['REMOTE_ADDR'].encode('utf-8'),name)
+		print >> f,"at time %s , the ua is : %s , the access ip : %s:%s , keyword is : %s".encode('utf-8')%(time.ctime(),request.META.get('HTTP_USER_AGENT',None),request.META['REMOTE_ADDR'],request.META.get('SERVER_PORT',None),name)
 	f.close()
 
 
